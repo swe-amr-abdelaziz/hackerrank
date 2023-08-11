83f5568df973b733e13c7eeda462c84bf2658799
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-08-11 18:44:09 +0300

    problem-solving/algorithms/time-conversion/

---
+++ problem-solving/algorithms/time-conversion/time-conversion.ts
@@ -0,0 +1,9 @@
+// https://www.hackerrank.com/challenges/time-conversion
+
+export default function timeConversion(s: string): string {
+  const dayNight: string = s.slice(-2);
+  let hour: number = parseInt(s.slice(0, 2), 10);
+  hour %= 12;
+  if (dayNight === 'PM') hour += 12;
+  return hour.toString().padStart(2, '0') + s.slice(2, -2);
+}
